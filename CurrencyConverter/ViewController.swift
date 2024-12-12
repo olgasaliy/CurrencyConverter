f6ca9d00917b384fb1e01325eb5aeafe6eb62c05
olgasaliy <olia.white.1996@gmail.com>
2024-12-12 09:53:20 +0100

    Create Xcode project

---
+++ CurrencyConverter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Olha Salii on 12.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
